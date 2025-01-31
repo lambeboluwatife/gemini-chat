bb2b2e90a4cbf1cfd7d40585d1f96ed01c94203f
lambeboluwatife <boluwatifelambe@gmail.com>
2025-01-31 17:50:37 +0100

    changed header name

---
+++ src/app/components/WelcomeMessage.jsx
@@ -0,0 +1,9 @@
+const WelcomeMessage = () => {
+  return (
+    <div className="flex flex-row mt-30 justify-center items-center text-white">
+      I am centered
+    </div>
+  );
+};
+
+export default WelcomeMessage;
